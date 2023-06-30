ad932fd3d00eb21bf0b1454f95199230b00d8f7e
AUNT1oN256 <anton.mironets@gmail.com>
2023-06-30 19:40:20 +0300

    add voices

--- JARVIS.py
+++ JARVIS.py
@@ -20,7 +20,29 @@ talk("Да, сер")
 
 def command():
     r = sr.Recognizer()
-    pass
+
+    with sr.Microphone() as source:
+        print("Say")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
 
 
-command()
+
+    task = r.recognize_google(audio, language="ru-RU").lower()
+    print("You: " + task)
+
+    return task
+
+
+def make_something(ar_task):
+    if "Открой" and "сайт" in ar_task:
+        talk("ok")
+        url = "https://youtube.com"
+        webbrowser.open(url)
+
+
+
+
+while True:
+    make_something(command())
